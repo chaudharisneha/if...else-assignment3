af0e520b37f3bb9545e5b1af869331f5ff61271a
chaudharisneha <snehachaudhari206@gmail.com>
2024-08-03 16:11:35 +0530

    assignment

---
+++ assignment_1.js
@@ -0,0 +1,9 @@
+let num = 7 
+if(num % 2 === 0){
+    console.log("Number is even");
+    }else{
+        console.log("Number is odd");
+        
+    }
+
+
---
+++ assignment_3.js
@@ -0,0 +1,98 @@
+    //Assignment 1
+    let num = 7 
+    if(num % 2 === 0){
+        console.log("Number is even");
+        }else{
+            console.log("Number is odd");
+            
+        }
+    //Assignment 2
+let score = 76
+if(score >= 90) {
+    console.log("A");
+    }else if(score >= 80) {
+        console.log("B");
+        }else if(score >= 70) {
+            console.log("C");
+            }else if(score >= 60) {
+                console.log("D");
+                }else{
+                    console.log("F");
+                    
+                }
+
+    //Assignment 3
+let age = 7;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+    //Assignment 4
+      let num1 = 7, num2 = 3, num3 = 9;
+      if (num1 > num2 && num1 > num3) {
+          console.log("The largest number is " + num1);
+      } else if (num2 > num1 && num2 > num3) {
+          console.log("The largest number is " + num2);
+      } else {
+          console.log("The largest number is " + num3);
+      }
+      
+    //Assignment 5
+let year = 1948;
+if(year%4 === 0){
+    console.log("This year is leap year");
+    }else{
+        console.log("This year is not leap year");
+        } 
+
+    //Assignment 6
+let number = 49;
+if(number%3 === 0 && number%5 ===0){
+    console.log("FizzBizz");
+    }else if(number%3 === 0){
+        console.log("Fizz");
+        }else if(number%5 === 0){
+            console.log("Bizz");
+            }else{
+                console.log("Number");
+                }
+
+     //Assignment 7          
+let hour = 24;
+if(hour >= 5 && hour <= 11){
+    console.log("Good Morning");
+    }else if(hour >= 12 && hour <= 17){
+        console.log("Good Afternoon");
+        }else if(hour >= 18 && hour <= 22){
+            console.log("Good Evening");
+             }else if((hour >= 23 && hour <= 24) || (hour >= 0 && hour <= 4)) {
+                console.log("Good Night");
+                }else {
+                    console.log("Invalid hour");
+                     }
+
+    //Assignment 8
+let ageIdentifier = 68;
+if(ageIdentifier >= 0 && ageIdentifier <= 12){
+    console.log("child");
+    }else if(ageIdentifier >= 13 && ageIdentifier <= 19){
+        console.log("teenager");
+    }else if(ageIdentifier >= 20 && ageIdentifier <= 64){
+        console.log("adult");
+        }else{
+            console.log("senior");
+            }
+    
+let color = "yellow";
+if(color === "red"){
+    console.log("Stop");
+    }else if(color === "yellow"){
+        console.log("Get Ready");
+        }else if(color === "green"){
+            console.log("Go");
+            }else{
+                console.log("Invalid");
+                 }
+
